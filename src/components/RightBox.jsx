fd2388dfafc7de21787af1a470e38d635830567c
mi hector <hectorlandaezopsu@gmail.com>
2023-10-21 20:23:05 -0500

    version 2.0 falta dr reactividad a las tarjetas

---
+++ src/components/RightBox.jsx
@@ -0,0 +1,25 @@
+import useCitie from "./useCitie";
+import TopBox from "./TopBox";
+
+function RightBox(){
+    const {data} = useCitie()
+
+    return(
+        <>
+        {data !== null ? (
+        <div className='rightBox'>
+        <div className='fmiddleBox'>
+          <div className='middleBox' >Wind status<span id="wind" >{data.wind.speed} mph</span></div>
+          <div className='middleBox'>Humidity<span id="humidity">{data.main.humidity} %</span></div>
+          </div>
+          <div className='fsmiddleBox'>
+          <div className='smiddleBox'>Visibility <span id="visibility">{data.visibility} miles</span></div>
+          <div className='smiddleBox'>Air Preasure<span id="air">{data.main.pressure} mb</span></div>
+          </div>
+        </div>
+        ) : null}
+        </>
+    )
+   
+}
+export default RightBox
