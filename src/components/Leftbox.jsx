52ef199977a0b33121e4dcc5f933ef30e47abd7d
mi hector <hectorlandaezopsu@gmail.com>
2023-10-23 20:45:37 -0500

    filtro de imagenes sirve

--- src/components/Leftbox.jsx
+++ src/components/Leftbox.jsx
@@ -5,31 +5,6 @@ import OCultar from "./Ocultar";
 function Leftbox(props) {
 const {data,future} = useCitie()
 
-const mimg = (root) => {
-  let imagen = "/src/assets/weather/LightCloud.png";
-    if (data.weather[root].main === "light rain") {
-      imagen = "/src/assets/weather/LightRain.png";
-    } else if (data.weather[root].main === "moderate rain") {
-      imagen = "/src/assets/weather/Shower.png";
-    } else if (data.weather[root].main === "overcast clouds") {
-      imagen = "/src/assets/weather/HeavyCloud.png";
-    } else if (data.weather[root].main === "broken clouds") {
-      imagen = "/src/assets/weather/LightCloud.png";
-    } else if (data.weather[root].main === "clear sky") {
-      imagen = "/src/assets/weather/Clear.png";
-    } else if (data.weather[root].main === "heavy rain") {
-      imagen = "/src/assets/weather/HeavyRain.png";
-    } else if (data.weather[root].main === "thunder storm") {
-      imagen = "/src/assets/weather/Thunderstorm.png";
-    } else if (data.weather[root].main === "sleet") {
-      imagen = "/src/assets/weather/Sleet.png";
-    } else if (data.weather[root].main === "light rain") {
-      imagen = "/src/assets/weather/LightRain.png";
-    } else if (data.weather[root].main === "snow") {
-      imagen = "/src/assets/weather/Snow.png";
-    }
-    return imagen;
-  } 
   const getCurrentDate = () => {
     const currentDate = new Date();
     const formattedDate = currentDate.toDateString();
@@ -39,12 +14,14 @@ return (
     <div>
       {data && future !== null ? (
         <div id='allLeftbox'>
+
           <div className="buttons">
             <button className="search" onClick={()=>OCultar('allLeftbox','firstDiv-Second')}>Search for places</button>
             <button className="btc">c</button>
           </div>
           <div className="leftInformation" id="letinfo">
-            <img src={mimg(0)} alt="" />
+{/*           <div className="fondo" ><img src='src/assets/weather/Cloud-background.png' alt=''></img></div>
+ */}            <img src={props.fimg} alt="" />
             <h1 className="temperature">{props.temp}°c</h1>
             <p className="climate">{props.weather}</p>
             <p className="day">today . {getCurrentDate()}</p>
@@ -57,4 +34,5 @@ return (
 }
 
 export default Leftbox
+
 
--- src/components/TopBox.jsx
+++ src/components/TopBox.jsx
@@ -3,33 +3,6 @@ import useCitie from "./useCitie";
 function TopBox(props) {
   const { future } = useCitie();
 
-  const mimg = (root) => {
-    let imagen = "/src/assets/weatherLightCloud.png";
-    if (future.list[root].weather[0].description === "light rain") {
-      imagen = "/src/assets/weather/LightRain.png";
-    } else if (future.list[root].weather[0].description === "moderate rain") {
-      imagen = "/src/assets/weather/Shower.png";
-    } else if (future.list[root].weather[0].description === "overcast clouds") {
-      imagen = "/src/assets/weather/HeavyCloud.png";
-    } else if (future.list[root].weather[0].description === "broken clouds") {
-      imagen = "/src/assets/weather/LightCloud.png";
-    } else if (future.list[root].weather[0].description === "clear sky") {
-      imagen = "/src/assets/weather/Clear.png";
-    } else if (future.list[root].weather[0].description === "heavy rain") {
-      imagen = "/src/assets/weather/HeavyRain.png";
-    } else if (future.list[root].weather[0].description === "thunder storm") {
-      imagen = "/src/assets/weatherThunderstorm.png";
-    } else if (future.list[root].weather[0].description === "sleet") {
-      imagen = "/src/assets/weather/Sleet.png";
-    } else if (future.list[root].weather[0].description === "light rain") {
-      imagen = "/src/assets/weather/LightRain.png";
-    } else if (future.list[root].weather[0].description === "snow") {
-      imagen = "/src/assets/weather/Snow.png";
-    }
-
-    return imagen;
-  };
-
   function convertirFecha(fechaStr) {
     const fecha = new Date(fechaStr);
 
@@ -49,33 +22,35 @@ function TopBox(props) {
         <div className="topBox">
           <div className="div1">
             tomorrow
-            <img src={mimg(2)} alt="time" />
+            <img src={props.mimg2} alt="time" />
             <span>
               {props.temp}°c {props.temp2}°c
             </span>
           </div>
           <div className="div1">
             {convertirFecha(future.list[10].dt_txt)}
-            <img src={mimg(10)} alt="time" />
-            <span>{`${props.temp8}°c${props.temp10}°c`}</span>
+            <img src={props.mimg10} alt="time" />
+            <span>
+            {`${props.temp8}°c${props.temp10}°c`}
+            </span>
           </div>
           <div className="div1">
             {convertirFecha(future.list[16].dt_txt)}
-            <img src={mimg(16)} alt="time" />
+            <img src={props.mimg16} alt="time" />
             <span>
               {props.temp16}°c {props.temp18}°c
             </span>
           </div>
           <div className="div1">
             {convertirFecha(future.list[26].dt_txt)}
-            <img src={mimg(26)} alt="time" />
+            <img src={props.mimg26} alt="time" />
             <span>
               {props.temp24}°c {props.temp26}°c
             </span>
           </div>
           <div className="div1">
             {convertirFecha(future.list[34].dt_txt)}
-            <img src={mimg(34)} alt="time" />
+            <img src={props.mimg34} alt="time" />
             <span>
               {props.temp32}°c {props.temp34}°c
             </span>
